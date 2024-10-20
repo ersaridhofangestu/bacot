bf51557ebdc9bcc00767e54cdc85517ef4454cad
ErsaRidhoFangestu <ersaridhof@gmail.com>
2024-10-20 21:14:33 +0700

    fix:routers 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GlobalStateProvider } from "./GlobalStateContext.jsx";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStateProvider>
       <Router>
-        <Route path="/" element={<App />} />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
       </Router>
     </GlobalStateProvider>
   </StrictMode>,
